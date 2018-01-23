165f82f234e9dd43ea3098f27cbe198f0b9a7955
korseby <kristian@korseby.net>
2018-01-23 18:13:36 +0100

    Update runTest1.sh

--- runTest1.sh
+++ runTest1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-R -e library(rDolphin);setwd(paste(system.file(package = "rDolphin"),"extdata",sep='/')); imported_data=import_data("Parameters_MTBLS242_15spectra_5groups.csv"); plot=exemplars_plot(imported_data)
+R -e 'library(rDolphin); setwd(paste(system.file(package = "rDolphin"),"extdata",sep="/")); imported_data=import_data("Parameters_MTBLS242_15spectra_5groups.csv"); plot=exemplars_plot(imported_data)'
