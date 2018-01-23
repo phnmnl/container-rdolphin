bca47e1411295860494165d62560bbd3c9b0a905
Daniel Cañueto <danielcanueto88@gmail.com>
2018-01-23 17:52:13 +0100

    Updated examples

--- runTest1.sh
+++ runTest1.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-R -e "library(rDolphin)"
-
+R -e library(rDolphin);setwd(paste(system.file(package = "rDolphin"),"extdata",sep='/')); imported_data=import_data("Parameters_MTBLS242_15spectra_5groups.csv"); plot=exemplars_plot(imported_data)
